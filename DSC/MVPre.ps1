dc90ed3236d08522f69234871a4f193de34807e5
Robert Dillon <bdillon007@gmail.com>
2024-05-19 16:15:08 -0400

    V2.1.25

--- DSC/MVPre.ps1
+++ DSC/MVPre.ps1
@@ -2,7 +2,7 @@ Configuration MVPre {
 
     Import-DSCResource -ModuleName 'PSDscResources'
 
-    # Version 2.1.19
+    # Version 2.1.25
     Script OS.Verify_SMB1ProtocolDisabled
     {
       # What: Disable (and remove) SMB1 protocol
