833f368b2462365e9a9f6bd8114a99d4b0e4a709
Robert Dillon <bdillon007@gmail.com>
2024-09-17 09:32:08 -0400

    Automation Account Variable Test 3

--- dcManufacturing/dcManufacturing.ps1
+++ dcManufacturing/dcManufacturing.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-Enforcement code for Manufacturing Computers Only - V5.0
+Enforcement code for Manufacturing Computers Only - V6.0
 
 .DESCRIPTION
 Enforcement code for Manufacturing Computers Only.  Enforces documented settings by using Desired State Configuration
